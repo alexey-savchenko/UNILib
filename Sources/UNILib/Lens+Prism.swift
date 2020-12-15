9c651958ac3f31a666ff5e6594c8b1a3b8a401d3
Alexey Savchenko <alexey.savchenko@gen.tech>
2020-12-15 22:42:14 +0200

    Added init to Lens and Prism

--- Sources/UNILib/Lens+Prism.swift
+++ Sources/UNILib/Lens+Prism.swift
@@ -11,42 +11,30 @@ import Foundation
 public struct Lens<Whole, Part> {
   let get: (Whole) -> Part
   let set: (Part) -> (Whole) -> Whole
+  
+  public init(
+    get: @escaping (Whole) -> Part,
+    set: @escaping (Part) -> (Whole) -> Whole
+  ) {
+    self.get = get
+    self.set = set
+  }
 }
 
 public struct Prism<Whole, Part> {
   let tryGet: (Whole) -> Part?
   let inject: (Part) -> Whole
-}
-
-public struct Affine<Whole, Part> {
-  let tryGet: (Whole) -> Part?
-  let trySet: (Part) -> (Whole) -> Whole?
-}
-
-public extension Affine {
-  func then <Subpart> (
-    _ other: Affine<Part, Subpart>
-  ) -> Affine<Whole, Subpart> {
-    
-    return Affine<Whole,Subpart>.init(
-      tryGet: { s in self.tryGet(s).flatMap(other.tryGet) },
-      trySet: { bp in
-        { s in
-          self.tryGet(s)
-            .flatMap { a in other.trySet(bp)(a) }
-            .flatMap { b in self.trySet(b)(s) }
-        }
-    })
+  
+  public init(
+    tryGet: @escaping (Whole) -> Part?,
+    inject: @escaping (Part) -> Whole
+  ) {
+    self.inject = inject
+    self.tryGet = tryGet
   }
 }
 
 public extension Lens {
-  func toAffine() -> Affine<Whole, Part> {
-    return Affine<Whole,Part>.init(
-      tryGet: self.get,
-      trySet: self.set)
-  }
-  
   func lift<Composite>(_ l: Lens<Composite, Whole>) -> Lens<Composite, Part> {
     return Lens<Composite, Part>(
       get: { (composite) -> Part in
@@ -56,7 +44,7 @@ public extension Lens {
           let subPart = self.set(subSubPart)(l.get(composite))
           return l.set(subPart)(composite)
         }
-    })
+      })
   }
 }
 
@@ -66,88 +54,92 @@ public extension Lens {
  
  -- LENS
  
+ import UNILib
+ 
  {% for type in types.structs|annotated:"lens" %}
  extension {{ type.name }} {
-   enum lens {
-     {% for variable in type.variables|!static|!computed %}
-     static let {{ variable.name }} = Lens<{{ type.name }}, {{ variable.typeName }}>(
-       get: { $0.{{ variable.name }} },
-       set: { part in
-         { whole in
-           {{ type.name }}.init({% for argument in type.variables|!static|!computed %}{{ argument.name }}: {% if variable.name == argument.name %}part{% else %}whole.{{ argument.name }}{% endif %}{% if not forloop.last %}, {% endif %}{% endfor %})
-         }
-       }
-     ){% endfor %}
-   }
+ enum lens {
+ {% for variable in type.variables|!static|!computed %}
+ static let {{ variable.name }} = Lens<{{ type.name }}, {{ variable.typeName }}>(
+ get: { $0.{{ variable.name }} },
+ set: { part in
+ { whole in
+ {{ type.name }}.init({% for argument in type.variables|!static|!computed %}{{ argument.name }}: {% if variable.name == argument.name %}part{% else %}whole.{{ argument.name }}{% endif %}{% if not forloop.last %}, {% endif %}{% endfor %})
+ }
+ }
+ ){% endfor %}
+ }
  }
  {% endfor %}
+ 
+ 
 
- 
- 
  -- PRISM
-
+ 
+ import UNILib
+ 
  {% macro getTypeName type %}{% if type.actualTypeName %}{{ type.actualTypeName.name }}{% else %}{{ type.typeName.name }}{% endif %}{% endmacro %}
-
+ 
  {%  for type in types.all where type.name == "SourceryImports_Prism" %}
  {% for variable in type.variables %}
  {% if variable|annotated:"testable" %}@testable {% else %}{% endif %}import {{ variable.name }}
  {% endfor %}
-
+ 
  {% endfor %}
-
+ 
  {% for type in types.enums|annotated:"prism" %}
-
+ 
  extension {{ type.name }} {
-     {{ type.accessLevel }} enum prism {
-         {% for case in type.cases %}
-     {% if type.isGeneric %}
-     {% if case.hasAssociatedValue %}
-     {{ type.accessLevel }} static var {{ case.name }}: Prism<{{ type.name }},{% if case.associatedValues.count > 1 %}({% for associated in case.associatedValues %}{% call getTypeName associated %}{% if forloop.last %}{% else %}, {% endif %}{% endfor %}){% else %}{% call getTypeName case.associatedValues.first %}{% endif %}> {
-       return .init(
-         tryGet: { if case .{{ case.name }}(let value) = $0 { return value } else { return nil } },
-         inject: { ({% for associated in case.associatedValues %}x{{ forloop.counter}}{% if not forloop.last%}, {% endif %}{% endfor %}) in .{{ case.name }}({% for associated in case.associatedValues %}{% if associated.localName != nil %}{{ associated.localName }}:{% endif %}x{{ forloop.counter }}{% if not forloop.last%}, {% endif %}{% endfor %}) })
-     }
-     {% else %}
-     {{ type.accessLevel }} static var {{ case.name }}: Prism<{{ type.name }}, ()> {
-       return .init(
-         tryGet: { if case .{{ case.name }} = $0 { return () } else { return nil } },
-         inject: { .{{ case.name }} })
-     }
-     {% endif %}
-     {% else %}
-         {% if case.hasAssociatedValue %}
-         {{ type.accessLevel }} static let {{ case.name }} = Prism<{{ type.name }},{% if case.associatedValues.count > 1 %}({% for associated in case.associatedValues %}{% call getTypeName associated %}{% if forloop.last %}{% else %}, {% endif %}{% endfor %}){% else %}{% call getTypeName case.associatedValues.first %}{% endif %}>(
-             tryGet: { if case .{{ case.name }}(let value) = $0 { return value } else { return nil } },
-             inject: { ({% for associated in case.associatedValues %}x{{ forloop.counter}}{% if not forloop.last%}, {% endif %}{% endfor %}) in .{{ case.name }}({% for associated in case.associatedValues %}{% if associated.localName != nil %}{{ associated.localName }}:{% endif %}x{{ forloop.counter }}{% if not forloop.last%}, {% endif %}{% endfor %}) })
-         {% else %}
-         {{ type.accessLevel }} static let {{ case.name }} = Prism<{{ type.name }}, ()>(
-             tryGet: { if case .{{ case.name }} = $0 { return () } else { return nil } },
-             inject: { .{{ case.name }} })
-         {% endif %}
-     {% endif %}
-
-         {% endfor %}
-     }
+ {{ type.accessLevel }} enum prism {
+ {% for case in type.cases %}
+ {% if type.isGeneric %}
+ {% if case.hasAssociatedValue %}
+ {{ type.accessLevel }} static var {{ case.name }}: Prism<{{ type.name }},{% if case.associatedValues.count > 1 %}({% for associated in case.associatedValues %}{% call getTypeName associated %}{% if forloop.last %}{% else %}, {% endif %}{% endfor %}){% else %}{% call getTypeName case.associatedValues.first %}{% endif %}> {
+ return .init(
+ tryGet: { if case .{{ case.name }}(let value) = $0 { return value } else { return nil } },
+ inject: { ({% for associated in case.associatedValues %}x{{ forloop.counter}}{% if not forloop.last%}, {% endif %}{% endfor %}) in .{{ case.name }}({% for associated in case.associatedValues %}{% if associated.localName != nil %}{{ associated.localName }}:{% endif %}x{{ forloop.counter }}{% if not forloop.last%}, {% endif %}{% endfor %}) })
  }
-
- {% if type.annotations.prism == "chain" %}
- {{ type.accessLevel }} extension PrismType where SType == TType, AType == BType, BType == {{ type.name }} {
-   {% for case in type.cases %}
-   {% if case.hasAssociatedValue %}
-   var {{ case.name }}: Prism<SType,{% if case.associatedValues.count > 1 %}({% for associated in case.associatedValues %}{% call getTypeName associated %}{% if forloop.last %}{% else %}, {% endif %}{% endfor %}){% else %}{% call getTypeName case.associatedValues.first %}{% endif %}> {
-     return self >>> {{ type.name }}.prism.{{ case.name }}
-   }
-   {% else %}
-   var {{ case.name }}: Prism<SType,()> {
-     return self >>> {{ type.name }}.prism.{{ case.name }}
-   }
-   {% endif %}
-
-   {% endfor %}
+ {% else %}
+ {{ type.accessLevel }} static var {{ case.name }}: Prism<{{ type.name }}, ()> {
+ return .init(
+ tryGet: { if case .{{ case.name }} = $0 { return () } else { return nil } },
+ inject: { .{{ case.name }} })
  }
  {% endif %}
-
+ {% else %}
+ {% if case.hasAssociatedValue %}
+ {{ type.accessLevel }} static let {{ case.name }} = Prism<{{ type.name }},{% if case.associatedValues.count > 1 %}({% for associated in case.associatedValues %}{% call getTypeName associated %}{% if forloop.last %}{% else %}, {% endif %}{% endfor %}){% else %}{% call getTypeName case.associatedValues.first %}{% endif %}>(
+ tryGet: { if case .{{ case.name }}(let value) = $0 { return value } else { return nil } },
+ inject: { ({% for associated in case.associatedValues %}x{{ forloop.counter}}{% if not forloop.last%}, {% endif %}{% endfor %}) in .{{ case.name }}({% for associated in case.associatedValues %}{% if associated.localName != nil %}{{ associated.localName }}:{% endif %}x{{ forloop.counter }}{% if not forloop.last%}, {% endif %}{% endfor %}) })
+ {% else %}
+ {{ type.accessLevel }} static let {{ case.name }} = Prism<{{ type.name }}, ()>(
+ tryGet: { if case .{{ case.name }} = $0 { return () } else { return nil } },
+ inject: { .{{ case.name }} })
+ {% endif %}
+ {% endif %}
+ 
  {% endfor %}
-
+ }
+ }
+ 
+ {% if type.annotations.prism == "chain" %}
+ {{ type.accessLevel }} extension PrismType where SType == TType, AType == BType, BType == {{ type.name }} {
+ {% for case in type.cases %}
+ {% if case.hasAssociatedValue %}
+ var {{ case.name }}: Prism<SType,{% if case.associatedValues.count > 1 %}({% for associated in case.associatedValues %}{% call getTypeName associated %}{% if forloop.last %}{% else %}, {% endif %}{% endfor %}){% else %}{% call getTypeName case.associatedValues.first %}{% endif %}> {
+ return self >>> {{ type.name }}.prism.{{ case.name }}
+ }
+ {% else %}
+ var {{ case.name }}: Prism<SType,()> {
+ return self >>> {{ type.name }}.prism.{{ case.name }}
+ }
+ {% endif %}
+ 
+ {% endfor %}
+ }
+ {% endif %}
+ 
+ {% endfor %}
+ 
  
  */
