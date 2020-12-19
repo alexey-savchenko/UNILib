3bed255546a3c8f779da2dc9a4ad4f484245cf35
Alexey Savchenko <alexey.savchenko@gen.tech>
2020-12-19 17:19:25 +0200

    Refactor PresentationContexr

--- Sources/UNILib/PresentationContext.swift
+++ Sources/UNILib/PresentationContext.swift
@@ -8,57 +8,100 @@
 #if canImport(UIKit)
 
 import UIKit
+//
+//public enum PresentationContext {
+//  case push(presentingController: UINavigationController)
+//  case modal(presentingController: UIViewController)
+//
+//  public var navigationController: UINavigationController? {
+//    switch self {
+//    case .push(let presentingController):
+//      return presentingController
+//    default:
+//      return nil
+//    }
+//  }
+//
+//  public func present(_ target: UIViewController, animated: Bool) {
+//    switch self {
+//    case .modal(let presentingController):
+//      presentingController.present(target,
+//                                   animated: animated,
+//                                   completion: nil)
+//    case .push(let presentingController):
+//      presentingController.pushViewController(target,
+//                                              animated: animated)
+//    }
+//  }
+//
+//  public func dismiss(_ target: UIViewController, animated: Bool) {
+//    switch self {
+//    case .modal:
+//      target.dismiss(animated: animated, completion: nil)
+//    case .push(let presentingController):
+//      presentingController.popViewController(animated: animated)
+//    }
+//  }
+//
+//  public func dismissToRoot(_ target: UIViewController, animated: Bool) {
+//    switch self {
+//    case .modal:
+//      target.dismiss(animated: animated, completion: nil)
+//    case .push(let presentingController):
+//      presentingController.popToRootViewController(animated: true)
+//    }
+//  }
+//
+//  public func dismissPresented(animated: Bool) {
+//    switch self {
+//    case .push(let presentingController):
+//      presentingController.popViewController(animated: animated)
+//    case .modal(let presentingController):
+//      presentingController.presentedViewController.do { $0.dismiss(animated: animated) }
+//    }
+//  }
+//}
 
-public enum PresentationContext {
-  case push(presentingController: UINavigationController)
-  case modal(presentingController: UIViewController)
+public protocol PresentationContext {
+  func present(_ controller: UIViewController, animated: Bool)
+  func dismiss(_ controller: UIViewController, animated: Bool)
+}
+
+public class PushPresentationContext: PresentationContext {
   
-  public var navigationController: UINavigationController? {
-    switch self {
-    case .push(let presentingController):
-      return presentingController
-    default:
-      return nil
-    }
+  let navigationController: UINavigationController
+  
+  public init(navigationController: UINavigationController) {
+    self.navigationController = navigationController
   }
   
-  public func present(_ target: UIViewController, animated: Bool) {
-    switch self {
-    case .modal(let presentingController):
-      presentingController.present(target,
-                                   animated: animated,
-                                   completion: nil)
-    case .push(let presentingController):
-      presentingController.pushViewController(target,
-                                              animated: animated)
-    }
+  public func present(_ controller: UIViewController, animated: Bool) {
+    navigationController.pushViewController(controller, animated: animated)
   }
   
-  public func dismiss(_ target: UIViewController, animated: Bool) {
-    switch self {
-    case .modal:
-      target.dismiss(animated: animated, completion: nil)
-    case .push(let presentingController):
-      presentingController.popViewController(animated: animated)
-    }
+  public func dismiss(_ controller: UIViewController, animated: Bool) {
+    navigationController.popViewController(animated: animated)
   }
   
-  public func dismissToRoot(_ target: UIViewController, animated: Bool) {
-    switch self {
-    case .modal:
-      target.dismiss(animated: animated, completion: nil)
-    case .push(let presentingController):
-      presentingController.popToRootViewController(animated: true)
-    }
+  public func popToRoot(animated: Bool) {
+    navigationController.popToRootViewController(animated: animated)
+  }
+}
+
+public class ModalPresentationContext: PresentationContext {
+  
+  let presentingController: UIViewController
+  
+  public init(presentingController: UIViewController) {
+    self.presentingController = presentingController
   }
   
-  public func dismissPresented(animated: Bool) {
-    switch self {
-    case .push(let presentingController):
-      presentingController.popViewController(animated: animated)
-    case .modal(let presentingController):
-      presentingController.presentedViewController.do { $0.dismiss(animated: animated) }
-    }
+  public func present(_ controller: UIViewController, animated: Bool) {
+    presentingController.present(controller, animated: animated)
+  }
+  
+  public func dismiss(_ controller: UIViewController, animated: Bool) {
+    controller.dismiss(animated: animated)
   }
 }
 
