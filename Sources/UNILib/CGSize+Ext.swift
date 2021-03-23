67697707a552ff5bd521f5e5412639d2ed50fecd
Alexey Savchenko <alexey.savchenko@gen.tech>
2021-03-23 14:40:52 +0200

    Added CGSize extension

---
+++ Sources/UNILib/CGSize+Ext.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alexey Savchenko on 23.03.2021.
+//
+
+import UIKit
+
+extension CGSize: Comparable {
+  public static func < (lhs: CGSize, rhs: CGSize) -> Bool {
+    return (lhs.width * lhs.height) < (rhs.width * rhs.height)
+  }
+}
